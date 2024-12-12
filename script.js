f52872fcce08661fa77e51fdc3c49243641fdd5a
Zsolt <184605676+Zsolt-qwerty@users.noreply.github.com>
2024-12-12 15:24:35 +0000

    streamlined functions

--- script.js
+++ script.js
@@ -11,32 +11,24 @@ async function retrieveCharacter() {
     }
 }
 
-retrieveCharacter();
-
 let qwert;
 
 async function display() {
     
     qwert = await retrieveCharacter();
-    // console.log(qwert);
-    updateDivs();
-}
 
-display();
-
-function updateDivs() {
     const img     = document.getElementById("img");
     const name    = document.getElementById("name");
     const species = document.getElementById("species");
     const status  = document.getElementById("status");
-
+    
     img.src = qwert.image;
     name.textContent = qwert.name;
     species.textContent = qwert.species;
     status.textContent = qwert.status;
-
 }
 
+// display();
 
 const btn = document.getElementById("btn");
 btn.addEventListener("click", (retrieveCharacter, display));
