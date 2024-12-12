fbed38094e4bbc5701d6f48506181bb41cce9afa
Zsolt <184605676+Zsolt-qwerty@users.noreply.github.com>
2024-12-12 15:10:26 +0000

    working logic

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ function random(num) {
 }
 
 async function retrieveCharacter() {
+    const randomId = random(826);
     try {
-        const response = await fetch("https://rickandmortyapi.com/api/character/2"); // look at the pages issue
+        const response = await fetch(`https://rickandmortyapi.com/api/character/${randomId}`); // look at the pages issue ✅
         const json = await response.json();
+        console.log(json);
         return json;
     }
     catch (error) {
@@ -15,11 +17,32 @@ async function retrieveCharacter() {
 
 retrieveCharacter();
 
+let qwert;
+
 async function display() {
     
-    const qwert = await retrieveCharacter();
-    console.log(qwert.name);
+    qwert = await retrieveCharacter();
+    console.log(qwert);
     // console.log(retrieveCharacter().image, json.name, json.species, json.status);
+    updateDivs();
 }
 
 display();
+
+function updateDivs() {
+    const img     = document.getElementById("img");
+    const name    = document.getElementById("name");
+    const species = document.getElementById("species");
+    const status  = document.getElementById("status");
+
+    img.src = qwert.image;
+    name.textContent = qwert.name;
+    species.textContent = qwert.species;
+    status.textContent = qwert.status;
+
+}
+
+
+const btn = document.getElementById("btn");
+btn.addEventListener("click", (retrieveCharacter, display));
+// btn.addEventListener("click", display);
