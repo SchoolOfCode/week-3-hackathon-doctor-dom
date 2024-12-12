b69df204a81a97ff659862c2e5db0424f4e081f2
Zsolt <184605676+Zsolt-qwerty@users.noreply.github.com>
2024-12-12 14:10:15 +0000

    fetching simple data working

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 function random(num) {
-    return Math.floor(Math.random() * num);
+    return Math.floor(Math.random() * num + 1);
 }
 
 async function retrieveCharacter() {
-    const response = await fetch("https://rickandmortyapi.com/api/character?page=42");
-      const json = await response.json();
-      return json;
+    try {
+        const response = await fetch("https://rickandmortyapi.com/api/character/2"); // look at the pages issue
+        const json = await response.json();
+        return json;
+    }
+    catch (error) {
+        console.error("error: ", error);
+    }
 }
 
-console.log(retrieveCharacter())
+retrieveCharacter();
+
+async function display() {
+    
+    const qwert = await retrieveCharacter();
+    console.log(qwert.name);
+    // console.log(retrieveCharacter().image, json.name, json.species, json.status);
+}
+
+display();
